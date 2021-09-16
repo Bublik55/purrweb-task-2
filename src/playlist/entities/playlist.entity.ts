f9f93c6a7ddd13b426b46551a7fbba2cd8e640dd
Mikhail <user211195@gmail.com>
2021-09-16 15:55:24 +0600

    missplace

--- src/playlist/entities/playlist.entity.ts
+++ src/playlist/entities/playlist.entity.ts
@@ -1,16 +1,14 @@
-import { Content } from "src/content/entities/content.entity";
-import { Display } from "src/display/entities/display.entity";
-import { Entity, ManyToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Content } from 'src/content/entities/content.entity';
+import { Display } from 'src/display/entities/display.entity';
+import { Entity, ManyToMany, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Playlist {
+  @PrimaryGeneratedColumn('uuid') id: string;
 
-	@PrimaryGeneratedColumn('uuid') id: string;
+  @OneToOne(() => Display, (display) => display.playlist)
+  display: Display;
 
-	@OneToOne(() => Display, display => display.playlist)
-	display: Display;
-
-	@ManyToMany(() => Content, content => content.playlists)
-	content: Content;
-
+  @ManyToMany(() => Content, (content) => content.playlists)
+  content: Content;
 }
