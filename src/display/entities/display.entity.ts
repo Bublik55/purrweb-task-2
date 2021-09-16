b3129737aeb35e5ebcaebb033c26c28143265930
Bublik55 <user211195@gmail.com>
2021-09-16 15:04:22 +0600

    Models look like prepeared Next:	UserModule 	AuthModule

--- src/display/entities/display.entity.ts
+++ src/display/entities/display.entity.ts
@@ -1,1 +1,17 @@
-export class Display {}
+import { Entity, ManyToOne, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Event } from "src/event/entities/event.entity";
+import { Playlist } from "src/playlist/entities/playlist.entity";
+
+@Entity()
+export class Display {
+
+	@PrimaryGeneratedColumn('uuid') id: string;
+
+
+
+	@ManyToOne(() => Event, event => event.displays )
+	event: Event;
+
+	@OneToOne(() => Playlist, playlist => playlist.display)
+	playlist: Playlist;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,50 @@
-export class User {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEAN, IsString, IsUUID } from "class-validator";
+import { randomUUID } from "crypto";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Event } from "src/event/entities/event.entity";
+
+
+@Entity()
+export class User {
+
+    @ApiProperty({
+        example: randomUUID,
+        type: String
+    })
+    @IsUUID()
+    @PrimaryGeneratedColumn('uuid') id: string;
+
+    @ApiProperty({
+        description: 'User`s name',
+        example: 'Bob',
+        type: String
+    })
+    @IsString()
+    @Column('text') name: string;
+
+    @ApiProperty({
+        description: "HASHED User's password",
+        example: "Awesomepasswd",
+        type: String
+    })
+    @IsString()
+    @Column('text') password: string;
+
+    @ApiProperty({
+        description: "User's email",
+        example: " Awesome@some.me",
+        type: String
+    })
+    @IsEAN()
+    @Column('text') email: string;
+
+    @ApiProperty({
+        description: "User's events",
+        example: Event,
+        type: Event
+    })
+    @OneToMany(() => Event, events => events.user)
+    events: Event[]
+
+}
