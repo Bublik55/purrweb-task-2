56a53c45cc79e11849ce1bad07222a2b85bbd0ae
Mikhail Krutov <user211195@gmail.com>
2021-09-27 02:50:10 +0600

    empty

--- src/database.providers.ts
+++ src/database.providers.ts
@@ -1,18 +1,18 @@
-import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { env } from "process";
-import { Content } from "./content/entities/content.entity";
-import { Display } from "./display/entities/display.entity";
-import { Event } from "./event/entities/event.entity";
-import { ContentToPlaylist } from "./playlist/entities/content-to-playlist.entity";
-import { Playlist } from "./playlist/entities/playlist.entity";
-import { User } from "./user/entities/user.entity";
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { env } from 'process';
+import { Content } from './content/entities/content.entity';
+import { Display } from './display/entities/display.entity';
+import { Event } from './event/entities/event.entity';
+import { ContentToPlaylist } from './playlist/entities/content-to-playlist.entity';
+import { Playlist } from './playlist/entities/playlist.entity';
+import { User } from './user/entities/user.entity';
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: "postgres",
+      type: 'postgres',
       host: env.DB_HOST,
       port: +env.DB_PORT,
       username: env.DB_USERNAME,
